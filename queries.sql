e499155c8ed708cfa698c14a98eb5062363ec442
nvytska <nvytska@kse.org.ua>
2025-05-27 13:33:47 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
-#1
-USE assignment_1;
 
+use assignment_1;
+
+#1 upcoming bookings where guests are in the blacklist
 select 
 g.name,
 g.surname,
@@ -16,19 +17,17 @@ from bookings b
 join guests g on b.guest_id = g.guest_id
 join rooms r on b.room_id = r.room_id
 join hotels h on r.hotel_id = h.hotel_id
-join payments p on b.booking_id = p.booking_id;
+join payments p on b.booking_id = p.booking_id
+where g.is_in_blacklist = true and b.check_in => current date
+order by b.check_in asc;
 
-#2
-select guest_id, name, surname, email
-from guests g 
-where is_in_blacklist = true;
 
-#3 using union
+#2 select "premium service" users, who ever booked 2+ times using union
 with all_bookings as(
 select * from bookings
 union all
-select * from archieved_bookings
-)
+select * from archieved_bookings)
+
 select
 g.name, g.surname,
 count(b.booking_id) as total_booked
@@ -38,26 +37,18 @@ left join payments p on p.booking_id = b.booking_id
 group by g.name, g.surname
 having total_booked  > 2;
 
- #4
-select 
-g.surname, g.name, g.email, 
-b.check_in
-from guests g
-join bookings b on b.guest_id = g.guest_id
-order by b.check_in;
 
-#5
+#3 getting top 1 apple pay payer
  with top_payments_by_applepay as(
  select g.guest_id, g.surname, g.name,
  p.amount, p.payment_method as method
+ 
  from guests g 
  join bookings b on b.guest_id = g.guest_id 
  join payments p on b.booking_id = p.booking_id
  where p.payment_method = 'apple pay'
  order by p.amount desc)
- 
  select * from top_payments_by_applepay 
  limit 1;
-
 
 
